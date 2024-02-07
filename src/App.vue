bfd049cbc154ccb418d7293314903c25928e1047
ymshim <ymshim@pmirnc.com>
2024-02-07 21:48:37 +0900

    뷰 역사적인 첫번째 수업

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="container">
+    <div v-for="dog in data" :key="dog">
+      <div v-if="dog.name !== '말티즈'">
+      <h5>{{ dog.name }}</h5>
+      <p>{{ dog.desc }}</p>
+    </div>
+    </div>
   </div>
 </template>
 
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+export default class APP extends Vue {
+  data = [
+    {
+      name: '말티즈',
+      desc: "귀여움"
+    },
+    {
+      name: '비숑',
+      desc: "겁나 귀엽다"
+    },
+    {
+      name: '리트리버',
+      desc: "착하다"
+    },
+    {
+      name: '불독',
+      desc: "무섭다"
+    }
+  ]
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  background-color: lightgray;
+  height: 500px;
+  display: flex;
+}
+
+.container div {
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
