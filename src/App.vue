334e09c728d38e872081edeae2a14f268b44854a
ymshim <ymshim@pmirnc.com>
2024-02-21 20:00:01 +0900

    data set2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="container">
+    <div v-for="food in worldFoodData" :key="food.name">
+      <img :src="food.img" :alt="food.name"/>
+      <h1>{{food.name}}</h1>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator";
+import {worldFoods} from "@/data/worldFoods";
 
 @Component
 export default class APP extends Vue {
+  worldFoodData = worldFoods;
 }
 </script>
-
 <style>
 body {
   margin: 0;
---
+++ src/data/worldFoods.ts
@@ -0,0 +1,99 @@
+type Food = {
+    name: string
+    national: string
+    category: string
+    img: string
+}
+
+export const worldFoods: Food[] = [
+    {
+        name: '찜갈비',
+        national: '한식',
+        category: '요리',
+        img: require('../../src/assets/imgs/찜갈비.jpg'),
+    },
+    {
+        name: '불고기',
+        national: '한식',
+        category: '요리',
+        img: require('../../src/assets/imgs/불고기.jpg'),
+    },
+    {
+        name: '비빔밥',
+        national: '한식',
+        category: '밥',
+        img: require('../../src/assets/imgs/비빔밥.jpg'),
+    },
+    {
+        name: '잔치국수',
+        national: '한식',
+        category: '면',
+        img: require('../../src/assets/imgs/잔치국수.jpg'),
+    },
+    {
+        name: '잡채',
+        national: '한식',
+        category: '면',
+        img: require('../../src/assets/imgs/잡채.jpg'),
+    },
+    {
+        name: '탕수육',
+        national: '중식',
+        category: '요리',
+        img: require('../../src/assets/imgs/탕수육.jpg'),
+    },
+    {
+        name: '깐풍기',
+        national: '중식',
+        category: '요리',
+        img: require('../../src/assets/imgs/깐풍기.jpg'),
+    },
+    {
+        name: '볶음밥',
+        national: '중식',
+        category: '밥',
+        img: require('../../src/assets/imgs/볶음밥.jpg'),
+    },
+    {
+        name: '짜장면',
+        national: '중식',
+        category: '면',
+        img: require('../../src/assets/imgs/짜장면.jpg'),
+    },
+    {
+        name: '짬뽕',
+        national: '중식',
+        category: '면',
+        img: require('../../src/assets/imgs/짬뽕.jpg'),
+    },
+    {
+        name: '나베',
+        national: '일식',
+        category: '요리',
+        img: require('../../src/assets/imgs/나베.jpg'),
+    },
+    {
+        name: '초밥',
+        national: '일식',
+        category: '밥',
+        img: require('../../src/assets/imgs/초밥.jpg'),
+    },
+    {
+        name: '텐동',
+        national: '일식',
+        category: '밥',
+        img: require('../../src/assets/imgs/텐동.jpg'),
+    },
+    {
+        name: '라멘',
+        national: '일식',
+        category: '면',
+        img: require('../../src/assets/imgs/라멘.jpg'),
+    },
+    {
+        name: '소바',
+        national: '일식',
+        category: '면',
+        img: require('../../src/assets/imgs/소바.jpg'),
+    },
+];
